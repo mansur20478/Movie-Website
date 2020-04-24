5aa0a5eabd7ff42f315f552fc93f04b72e70e35d
Mansur <mansur20478@yandex.ru>
2020-04-24 17:28:31 +0600

    Пример

---
+++ data/users.py
@@ -0,0 +1,26 @@
+import datetime
+import sqlalchemy
+from flask_login import UserMixin
+from sqlalchemy_serializer import SerializerMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from .db_session import SqlAlchemyBase
+
+
+class Users(SqlAlchemyBase, UserMixin, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    nickname = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String, index=True, unique=True, nullable=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    photo_url = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    access_level = sqlalchemy.Column(sqlalchemy.Integer, default=0)
+    modified_date = sqlalchemy.Column(sqlalchemy.Integer, default=datetime.datetime.utcnow())
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
--- main.py
+++ main.py
@@ -26,9 +26,25 @@ def login():
     return render_template("login.html", form=form)
 
 
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+
+
 @app.route("/film_page")
 def film_page():
-    return render_template("film_page.html")
+    film_info = {
+        'photo_url': 'https://gidonline.io/img/9b85f7218_200x300.jpg',
+        'title': 'title',
+        'year': 'year',
+        'country': 'country',
+        'genre': 'genre',
+        'duration': 'duration',
+        'description': 'description',
+        'score': 10,
+        'film_url': 'https://stonehenge.load.hdrezka-ag.net/tvseries/f455776e70b462a514403e5dae056fc894ca5c92/a2b31d7e35a4d1a4bb024721e145d494:2020042511/240.mp4'
+    }
+    return render_template("film_page.html", film=film_info)
 
 
 if __name__ == '__main__':
